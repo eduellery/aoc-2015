fff8e6a2b2ba18867191db0a9605b0528e0add48
Eduardo Ellery <eduellery@gmail.com>
2021-12-14 21:31:35 +0100

    Setup

---
+++ src/aoc/day_00.py
@@ -0,0 +1,10 @@
+class Day0():
+    answers = [1, 2]
+
+
+def solve_1() -> int:
+    return 1
+
+
+def solve_2() -> int:
+    return 2
---
+++ tests/test_day_00.py
@@ -0,0 +1,9 @@
+from aoc.day_00 import solve_1, solve_2
+
+
+def test_solve_1():
+    assert solve_1() is 1
+
+
+def test_solve_2():
+    assert solve_2() is 2
