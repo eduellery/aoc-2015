84f5e02f9706157defd08a7db5b1aefdb379ce32
Eduardo Ellery <eduellery@gmail.com>
2021-12-14 21:34:21 +0100

    Flake8

--- tests/test_day_00.py
+++ tests/test_day_00.py
@@ -2,8 +2,8 @@ from aoc.day_00 import solve_1, solve_2
 
 
 def test_solve_1():
-    assert solve_1() is 1
+    assert solve_1() == 1
 
 
 def test_solve_2():
-    assert solve_2() is 2
+    assert solve_2() == 2
