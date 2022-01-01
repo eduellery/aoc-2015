90937f2fafb004edc779f23235b9676f75df80f6
Eduardo Ellery <eduellery@gmail.com>
2022-01-01 21:21:03 +0100

    2015 - Day 05

--- src/aoc/day_05.py
+++ src/aoc/day_05.py
@@ -9,14 +9,14 @@ class Day05:
     def solve1(self, nice: int = 0) -> int:
         for line in self.lines:
             if len(findall(r"[aeiou]", line)) >= 3 \
-                    and len(findall(r"([a-z])\1", line)) >= 1 \
+                    and search(r"([a-z])\1", line) \
                     and not search(r"ab|cd|pq|xy", line):
                 nice += 1
         return nice
 
     def solve2(self, nice: int = 0) -> int:
         for line in self.lines:
-            if findall(r'([a-z]{2}).*\1', line) \
-                    and findall(r'([a-z]).\1', line):
+            if search(r'([a-z]{2}).*\1', line) \
+                    and search(r'([a-z]).\1', line):
                 nice += 1
         return nice
