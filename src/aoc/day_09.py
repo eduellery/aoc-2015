9efba6560a9442a5acf51225ac044e690e777ad0
Eduardo Ellery <eduellery@gmail.com>
2022-01-04 06:56:59 +0100

    2015 - Day 09

---
+++ src/aoc/day_09.py
@@ -0,0 +1,35 @@
+from itertools import permutations
+from re import findall
+from typing import List
+
+
+class Day09:
+    def __init__(self, costs: List[str]):
+        distances = {}
+        cities = set()
+
+        for cost in costs:
+            travel = findall(r'(.*)\sto\s(.*)\s=\s(.*)', cost)
+            for orig, dest, dist in travel:
+                distances[orig, dest] = dist
+                distances[dest, orig] = dist
+                cities.add(orig)
+                cities.add(dest)
+
+        self.minimum = 10 ** 15
+        self.maximum = 0
+
+        for path in permutations(cities):
+            current = 0
+            for i in range(1, len(path)):
+                current += int(distances[path[i - 1], path[i]])
+            if current < self.minimum:
+                self.minimum = current
+            if current > self.maximum:
+                self.maximum = current
+
+    def solve1(self) -> int:
+        return self.minimum
+
+    def solve2(self) -> int:
+        return self.maximum
---
+++ test/test_day_09.py
@@ -0,0 +1,21 @@
+from aoc.day_09 import Day09
+from resources import read_as_string_list
+
+test_day = Day09(['London to Dublin = 464', 'London to Belfast = 518', 'Dublin to Belfast = 141'])
+input_day = Day09(read_as_string_list('test/day09.in'))
+
+
+def test_solve_1_example():
+    assert test_day.solve1() == 605
+
+
+def test_solve_1_input():
+    assert input_day.solve1() == 117
+
+
+def test_solve_2_example():
+    assert test_day.solve2() == 982
+
+
+def test_solve_2_input():
+    assert input_day.solve2() == 909
