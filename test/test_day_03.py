c0188ca1d8c5b309c39630c87df779cc0392ec64
Eduardo Ellery <eduellery@gmail.com>
2022-01-11 21:02:32 +0100

    2015 - Day 03

--- test/test_day_03.py
+++ test/test_day_03.py
@@ -3,7 +3,7 @@ from aoc.day_03 import Day03
 
 from resources import read_as_string
 
-aoc_input: str = read_as_string('test/day03.in')
+aoc_input = Day03(read_as_string('test/day03.in'))
 
 
 @pytest.mark.parametrize("test_input, expected", [
@@ -16,7 +16,7 @@ def test_solve_1_examples(test_input: str, expected: int):
 
 
 def test_solve_1_input():
-    assert Day03(aoc_input).solve1() == 2_565
+    assert aoc_input.solve1() == 2_565
 
 
 @pytest.mark.parametrize("test_input, expected", [
@@ -29,4 +29,4 @@ def test_solve_2_examples(test_input: str, expected: int):
 
 
 def test_solve_2_input():
-    assert Day03(aoc_input).solve2() == 2_639
+    assert aoc_input.solve2() == 2_639
