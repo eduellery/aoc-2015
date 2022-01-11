acccb4fa5a089912d74a3d3ac3d22c5cc9ad7ef8
Eduardo Ellery <eduellery@gmail.com>
2022-01-11 21:04:18 +0100

    2015 - Day 05

--- test/test_day_05.py
+++ test/test_day_05.py
@@ -5,7 +5,7 @@ from aoc.day_05 import Day05
 
 from resources import read_as_string_list
 
-aoc_input: List[str] = read_as_string_list('test/day05.in')
+aoc_input = Day05(read_as_string_list('test/day05.in'))
 
 
 @pytest.mark.parametrize("test_input, expected", [
@@ -17,7 +17,7 @@ def test_solve_1_examples(test_input: List[str], expected: int):
 
 
 def test_solve_1_input():
-    assert Day05(aoc_input).solve1() == 255
+    assert aoc_input.solve1() == 255
 
 
 @pytest.mark.parametrize("test_input, expected", [
@@ -29,4 +29,4 @@ def test_solve_2_examples(test_input: List[str], expected: int):
 
 
 def test_solve_2_input():
-    assert Day05(aoc_input).solve2() == 55
+    assert aoc_input.solve2() == 55
