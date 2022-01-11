ad005d3dcf0b37a6963700ad3e253bbcd15f99eb
Eduardo Ellery <eduellery@gmail.com>
2022-01-11 21:09:24 +0100

    2015 - Day 08

--- test/test_day_08.py
+++ test/test_day_08.py
@@ -1,23 +1,22 @@
-from typing import List
-
 from aoc.day_08 import Day08
+
 from resources import read_as_string_list
 
-test_input: List[str] = ['""', '"abc"', '"aaa\\"aaa"', '"\\x27"']
-aoc_input: List[str] = read_as_string_list('test/day08.in')
+test_input = Day08(['""', '"abc"', '"aaa\\"aaa"', '"\\x27"'])
+aoc_input = Day08(read_as_string_list('test/day08.in'))
 
 
 def test_solve_1_example():
-    assert Day08(test_input).solve1() == 12
+    assert test_input.solve1() == 12
 
 
 def test_solve_1_input():
-    assert Day08(aoc_input).solve1() == 1_333
+    assert aoc_input.solve1() == 1_333
 
 
 def test_solve_2_example():
-    assert Day08(test_input).solve2() == 19
+    assert test_input.solve2() == 19
 
 
 def test_solve_2_input():
-    assert Day08(aoc_input).solve2() == 2_046
+    assert aoc_input.solve2() == 2_046
