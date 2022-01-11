479958065c09d77b4d82a3c80957ead451a1af6e
Eduardo Ellery <eduellery@gmail.com>
2022-01-11 21:07:21 +0100

    2015 - Day 07

--- test/test_day_07.py
+++ test/test_day_07.py
@@ -1,14 +1,13 @@
-from typing import List
-
 from aoc.day_07 import Day07
+
 from resources import read_as_string_list
 
-aoc_input: List[str] = read_as_string_list('test/day07.in')
+aoc_input = Day07(read_as_string_list('test/day07.in'))
 
 
 def test_solve_1_input():
-    assert Day07(aoc_input).solve1() == 16_076
+    assert aoc_input.solve1() == 16_076
 
 
 def test_solve_2_input():
-    assert Day07(aoc_input).solve2() == 2_797
+    assert aoc_input.solve2() == 2_797
