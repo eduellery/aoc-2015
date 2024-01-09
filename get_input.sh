d846edbbe0bacefe333efe4a0a5fd5889c37c583
Eduardo Ellery <eduellery@gmail.com>
2024-01-09 19:51:26 +0100

    Add script to retrieve inputs

---
+++ get_input.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+YR=2015
+DAY=$1
+
+if [ -z "$DAY" ]; then
+    DAY=$(date +%e | tr -d " ")
+fi
+
+if [ -r .aocsession ]; then
+    AOC_SESSION=$(cat .aocsession)
+fi
+
+if [ -z "$AOC_SESSION" ]; then
+    echo "no session found"
+    exit 1
+fi
+
+D2=$(printf "%02d" $DAY)
+
+echo "getting puzzle input for day $D2 for year $YR"
+
+(
+UA="https://github.com/eduellery/aoc-2015"
+curl -s https://adventofcode.com/$YR/day/$DAY/input --cookie "session=$AOC_SESSION" -H "User-Agent: $UA"
+) | awk 'NR>1{print PREV} {PREV=$0} END{printf("%s",$0)}' > test/day$D2.in
