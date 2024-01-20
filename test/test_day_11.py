479618cdd36ce50964c501d7846db0dc84fc8a1b
Eduardo Ellery <eduellery@gmail.com>
2024-01-20 11:32:58 +0100

    Refactor Day 11

--- test/test_day_11.py
+++ test/test_day_11.py
@@ -20,6 +20,14 @@ def test_solve_1_input():
     assert aoc_input.solve1() == 'hxbxxyzz'
 
 
+@pytest.mark.parametrize("test_input, expected", [
+    ('abcdefgh', 'abcdffbb'),
+    ('ghijklmn', 'ghjbbcdd'),
+])
+def test_solve_2_examples(test_input: str, expected: str):
+    assert Day11(test_input).solve2() == expected
+
+
 @pytest.mark.skipif(not local_test, reason = 'Input files can not be shared')
 def test_solve_2_input():
     assert aoc_input.solve2() == 'hxcaabcc'
