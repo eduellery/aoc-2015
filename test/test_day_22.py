79788467a86d44b5632721e3ce8388a840879ef0
Eduardo Ellery <eduellery@gmail.com>
2024-01-24 07:17:08 +0100

    Refactor Day 22

--- test/test_day_22.py
+++ test/test_day_22.py
@@ -3,13 +3,22 @@ from aoc.day_22 import Day22
 
 from resources import file_exists
 
+test_input = Day22(10, 250, 13, 8)
 local_test =  file_exists('test/day22.in')
 aoc_input = Day22(50, 500, 71, 10)
+
+
+def test_solve_1_example():
+    assert test_input.solve1() == 226
 
 
 @pytest.mark.skipif(not local_test, reason = 'Input files can not be shared')
 def test_solve_1_input():
     assert aoc_input.solve1() == 1_824
+
+
+def test_solve_2_example():
+    assert test_input.solve2() == 226
 
 
 @pytest.mark.skipif(not local_test, reason = 'Input files can not be shared')
