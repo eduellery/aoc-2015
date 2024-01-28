4ea6e27abe6f5b97ae008a2ab99acaaf8999f91f
Eduardo Ellery <eduellery@gmail.com>
2024-01-28 05:05:16 +0100

    Add testcase to Day 07

--- test/test_day_07.py
+++ test/test_day_07.py
@@ -3,13 +3,14 @@ from aoc.day_07 import Day07
 
 from resources import read_as_string_list, file_exists
 
-test_input = Day07(read_as_string_list('test/day07.example'))
+test1_input = Day07(read_as_string_list('test/day07.part1.example'))
+test2_input = Day07(read_as_string_list('test/day07.part2.example'))
 local_test =  file_exists('test/day07.in')
 aoc_input = Day07(read_as_string_list('test/day07.in')) if local_test else None
 
 
 def test_solve_1_example():
-    assert test_input.solve1() == 123
+    assert test1_input.solve1() == 123
 
 
 @pytest.mark.skipif(not local_test, reason = 'Input files can not be shared')
@@ -18,7 +19,7 @@ def test_solve_1_input():
 
 
 def test_solve_2_example():
-    assert test_input.solve2() == 123
+    assert test2_input.solve2() == 4616
 
 
 @pytest.mark.skipif(not local_test, reason = 'Input files can not be shared')
